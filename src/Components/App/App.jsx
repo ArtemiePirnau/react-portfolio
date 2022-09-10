cd679f5d4afd1fc42b41c287df8e10e37f38736a
Pirnau Artemie <artemiepirnau@gmail.com>
2022-09-10 18:13:27 +0300

    Added something

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -5,17 +5,6 @@ import MySkills from "../MySkills/MySkills.jsx";
 import Portfolio from "../Portfolio/Portfolio.jsx";
 import Footer from "../Footer/Footer.jsx";
 
-// READY!
-// TODO: React form - cumva de primit message pe gmail din forma - //TODO: GATA!!!
-// TODO: De adaugat Skill cu react/redux - //TODO: GATA!!!
-// TODO: De sters toate comentariile - //TODO: GATA!!!
-
-
-// DE FACUT
-
-// TODO: De curatit input value dupa ce facem submit la form
-// TODO: De pus acest proiect pe un hosting free cu un domen de al meu de exemplu : (https://whoisartemie)
-
 import "../../style/style.scss";
 
 const App = () => {
